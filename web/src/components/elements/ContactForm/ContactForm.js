5f9f1e6e5d12f2c5d14bc55b9ed2422a5dbfee4c
Jorge Brisa <jorge.brisa@xing.com>
2020-10-31 00:17:18 +0100

    hides recaptcha

--- web/src/components/elements/ContactForm/ContactForm.js
+++ web/src/components/elements/ContactForm/ContactForm.js
@@ -72,6 +72,17 @@ export function ContactForm({
           action="report"
           verifyCallback={verifyCallback}
         />
+        <small>
+          This site is protected by reCAPTCHA and the Google{" "}
+          <a href="https://policies.google.com/privacy" target="_blank">
+            Privacy Policy
+          </a>{" "}
+          and{" "}
+          <a href="https://policies.google.com/terms" target="_blank">
+            Terms of Service
+          </a>{" "}
+          apply.
+        </small>
       </div>
 
       <div className="form-group">
--- web/src/components/layout/NavItem/NavItem.js
+++ web/src/components/layout/NavItem/NavItem.js
@@ -8,7 +8,16 @@ const NavItem = () => {
     <Fragment>
       <ul className="navbar-nav">
         <li>
+          <NavLink to={pageRoutes.home()}>Home</NavLink>
+        </li>
+        <li>
           <NavLink to={pageRoutes.home()}>Find IPOs</NavLink>
+        </li>
+        <li>
+          <NavLink to={pageRoutes.about()}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={pageRoutes.contact()}>Contact</NavLink>
         </li>
       </ul>
     </Fragment>
